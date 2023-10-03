a5af7b3131d0f976e1934359f9ced16484a8fdad
Luis Velito <luisvelito@Luiss-Mac-Studio.local>
2023-10-03 02:07:46 -0500

    crud

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,34 +9,36 @@ exports.signin = async (req, res) => {
     const user = db.get('users').find({ email: req.body.email, password: req.body.password }).value();
 
     if (!user) {
-        return res.status(404).send({ message: 'Error in login...' });
+      return res.status(404).send({ message: 'Error in login...' });
     }
 
     const token = jwt.sign(
-        { 
-            id: user.id 
-        },
-        config.secret,
-        {
-            algorithm: 'HS256',
-            allowInsecureKeySizes: true,
-            expiresIn: 86400, // 24 hours
-        }
+      {
+        id: user.id
+      },
+      config.secret,
+      {
+        algorithm: 'HS256',
+        allowInsecureKeySizes: true,
+        expiresIn: 86400, // 24 hours
+      }
     );
-    
+
     // store token
     req.session.token = token;
-    
+    res.setHeader('Access-Control-Allow-Origin', 'https://pacifica-lvelitoruiz.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     return res
-        .status(200)
-        .send(
-            {
-              token: token,
-              username: user.email,
-              displayname:  user.name,
-              initials: user.initials,
-            }
-        );
+      .status(200)
+      .send(
+        {
+          token: token,
+          username: user.email,
+          displayname: user.name,
+          initials: user.initials,
+        }
+      );
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
@@ -46,7 +48,7 @@ exports.signout = async (req, res) => {
   try {
     req.session = null;
 
-    return res.status(200).send( { message: "Signed out..." } );
+    return res.status(200).send({ message: "Signed out..." });
   } catch (err) {
     this.next(err);
   }
@@ -58,19 +60,21 @@ exports.signup = async (req, res) => {
 
   if (user) {
 
-      const entity = db.get('users').takeRight(1).value()[0];
-      const id = ((entity) ? (entity.id + 1) : 0);
-  
-      user = {
-          ...user,
-          id: id
-      };
+    const entity = db.get('users').takeRight(1).value()[0];
+    const id = ((entity) ? (entity.id + 1) : 0);
 
-      db.get('users').push(user).write();
+    user = {
+      ...user,
+      id: id
+    };
 
-      res.sendStatus(200);
+    db.get('users').push(user).write();
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://pacifica-lvelitoruiz.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
   } else {
 
-      res.status(500).send('uncaught error....');
+    res.status(500).send('uncaught error....');
   }
 };
